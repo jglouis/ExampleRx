a3a1c1df9891a57e521ceaea100219cbaf96bf5d
jgl <jgl@spaceapplications.com>
2015-05-29 14:28:42 +0200

    Tested flatmap.

--- src/main/java/be/jgl/Example.java
+++ src/main/java/be/jgl/Example.java
@@ -1,6 +1,12 @@
 package be.jgl;
 
 import rx.Observable;
+import rx.Subscriber;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Random;
+import java.util.concurrent.Future;
 
 /**
  * Created by jgl on 29/05/2015.
@@ -13,5 +19,18 @@ class Example {
                 .map(String::hashCode)
                 .map(i -> Integer.toString(i))
                 .subscribe(System.out::println);
+
+        Collection<Integer> ints = getRandInts(10);
+        Observable<Collection<Integer>> obsInts = Observable.just(ints);
+
+        obsInts.subscribe(System.out::println);
+        obsInts.flatMap(c -> Observable.from(c)).map(i -> i + 1).subscribe(System.out::println);
+    }
+
+    private static Collection<Integer> getRandInts(int n) {
+        Collection<Integer> collection = new ArrayList<>();
+        for (int i = 0; i < n; i++)
+            collection.add(new Random().nextInt());
+        return collection;
     }
 }
